e1abd7483ca895c6625546d11be5b972a05a65f9
Jessica Melero <jessicameleromartinez@MacBook-Pro-de-Jessica-4.local>
2018-10-25 14:00:33 +0200

    Ejercicio 2 completado

--- 1-bucles/2-bucle-for.js
+++ 1-bucles/2-bucle-for.js
@@ -1,3 +1,12 @@
 // ej 2
 // haz un bucle que muestre por consola los impares del 3 al 17 inclusive
 // Utilizad el bucle for
+
+var impar;
+
+for ( var i=3; i<18; i++ ) {
+    if ( i % 2 !== 0 ){
+        impar = i;
+        console.log(impar);
+    }
+}
