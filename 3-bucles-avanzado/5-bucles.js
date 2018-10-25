f3d6b6803711410380a1f2f8a6dad6bbd1601ede
Jessica Melero <jessicameleromartinez@MBPdeJessica4.home>
2018-10-25 22:17:13 +0200

    Ejercicio 5 bucles avanzados completado

--- 3-bucles-avanzado/5-bucles.js
+++ 3-bucles-avanzado/5-bucles.js
@@ -18,6 +18,11 @@ console.log('El array original es:  arr =', arr);
 
 // Aquí tu código
 
+var total=0;
+
+for (var i of arr) {
+    total+= i;
+}
 
 // Hasta aquí
 console.log('El total nuevo es: ', total);
